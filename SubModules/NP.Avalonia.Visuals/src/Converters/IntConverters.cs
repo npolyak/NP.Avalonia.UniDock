ce959fed7710162cac9d5aab29df379a8fad9a31
npolyak <nick.polyak@verizon.net>
2021-08-26 17:06:50 -0400

    added copyright notice to the source files

--- SubModules/NP.Avalonia.Visuals/src/Converters/IntConverters.cs
+++ SubModules/NP.Avalonia.Visuals/src/Converters/IntConverters.cs
@@ -1,4 +1,15 @@
-﻿namespace NP.Avalonia.Visuals.Converters
+﻿// (c) Nick Polyak 2021 - http://awebpros.com/
+// License: MIT License (https://opensource.org/licenses/MIT)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author of this software if something goes wrong. 
+// 
+// Also, please, mention this software in any documentation for the 
+// products that use it.
+
+namespace NP.Avalonia.Visuals.Converters
 {
     public static class IntConverters
     {
--- SubModules/NP.Avalonia.Visuals/src/Converters/ToBoolConverter.cs
+++ SubModules/NP.Avalonia.Visuals/src/Converters/ToBoolConverter.cs
@@ -1,4 +1,15 @@
-﻿using Avalonia.Data.Converters;
+﻿// (c) Nick Polyak 2021 - http://awebpros.com/
+// License: MIT License (https://opensource.org/licenses/MIT)
+//
+// short overview of copyright rules:
+// 1. you can use this framework in any commercial or non-commercial 
+//    product as long as you retain this copyright message
+// 2. Do not blame the author of this software if something goes wrong. 
+// 
+// Also, please, mention this software in any documentation for the 
+// products that use it.
+
+using Avalonia.Data.Converters;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
