c04558fe347b48e2e44b269af0162c2fc7708bca
npolyak <nick.polyak@verizon.net>
2021-10-14 13:57:49 -0400

    added view model sample

---
+++ Prototypes/NP.ViewModelSample/MyContainer.cs
@@ -0,0 +1,19 @@
+﻿using NP.Avalonia.UniDock.Factories;
+using NP.IoCy;
+
+namespace NP.ViewModelSample
+{
+    public static class MyContainer
+    {
+        public static IoCContainer? TheContainer { get; }
+
+        static MyContainer()
+        {
+            TheContainer = new IoCContainer();
+
+            TheContainer.Map<IFloatingWindowFactory, MyCustomFloatingWindowFactory>();
+
+            TheContainer?.CompleteConfiguration();
+        }
+    }
+}
